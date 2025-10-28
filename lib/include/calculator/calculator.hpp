18f20719ff74d50814654add7ae14a8617c679c8
Ilya Gellert <gellertilya@gmail.com>
2025-10-28 23:01:24 +0300

    feat(lib): refactor class Calculator

--- lib/include/calculator/calculator.hpp
+++ lib/include/calculator/calculator.hpp
@@ -1,4 +1,25 @@
 #ifndef CALCULATOR_CALCULATOR_HPP
 #define CALCULATOR_CALCULATOR_HPP
 
+#include "calculator/i_calculator_registrar.hpp"
+
+#include <string>
+#include <vector>
+#include <memory>
+
+class Calculator : public ICalculatorRegistrar {
+    public:
+    Calculator();
+
+    ~Calculator();
+
+    void loadPlugins(const std::string& pluginDir);
+    double evaluate(const std::string& expression);
+
+    void registerOperation(const OperationInfo& operation) override;
+private:
+    class Impl;
+    std::unique_ptr<Impl> pImpl;
+};
+
 #endif //CALCULATOR_CALCULATOR_HPP
