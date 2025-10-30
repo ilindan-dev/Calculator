ec790419feef67601d888c39c3de2f856b981af8
Ilya Gellert <gellertilya@gmail.com>
2025-10-30 23:00:22 +0300

    feat(plugin-ln): add ln plugin

--- plugins/ln/src/ln.cpp
+++ plugins/ln/src/ln.cpp
@@ -1,3 +1,22 @@
-//
-// Created by gellertilya on 30.10.2025.
-//
+#include "calculator/plugin.hpp"
+#include <cmath>
+
+double ln_func(const double* args, int argCount) {
+    if (argCount != 1) {
+        return NAN;
+    }
+
+    if (args[0] <= 0) return NAN;
+    return std::log(args[0]);
+}
+
+extern "C" PLUGIN_API void registerPlugin(ICalculatorRegistrar* registrar) {
+    OperationInfo info = {};
+    info.canonicalName = "ln";
+    info.function = ln_func;
+    info.numArguments = 1;
+    info.type = OperationType::Function;
+    info.symbol = "ln";
+
+    registrar->registerOperation(info);
+}
